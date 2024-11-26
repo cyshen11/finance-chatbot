170e6f6e789a0a357583b0bf11aca71c2b1f3850
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-11-26 22:16:16 +0800

    added langchain-community library

--- components/utils.py
+++ components/utils.py
@@ -1,6 +1,12 @@
 from langchain_community.document_loaders.sharepoint import SharePointLoader
 from dotenv import load_dotenv
+import os
 
+# Load environment variables from the .env file
 load_dotenv()
 
-loader = SharePointLoader(document_library_id="YOUR DOCUMENT LIBRARY ID")
+def auth_sharepoint():
+  loader = SharePointLoader(document_library_id=os.getenv("DOCUMENT_LIBRARY_ID"), auth_with_token=True)
+
+if __name__ == "__main__":
+  auth_sharepoint()

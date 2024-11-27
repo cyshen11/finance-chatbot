8507a84ca8aa5c742b93b6ce3500ad83f4aa6d92
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-11-27 15:16:11 +0800

    added function to create vector search index

--- components/index_doc.py
+++ components/index_doc.py
@@ -6,6 +6,8 @@ import tiktoken
 import warnings
 from openai import OpenAI
 from pymongo import MongoClient
+from pymongo.operations import SearchIndexModel
+import time
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -44,6 +46,41 @@ def insert_docs_to_mongodb(docs_to_insert):
 
   return result
 
+def create_vector_index():
+  # Connect to your Atlas cluster
+  client = MongoClient(os.getenv("MONGODB_CONNECTION_STRING"))
+  collection = client[os.getenv("MONGODB_DB_NAME")][os.getenv("MONGODB_DB_COLLECTION")]
+
+  # Create your index model, then create the search index
+  index_name="vector_index"
+  search_index_model = SearchIndexModel(
+    definition = {
+      "fields": [
+        {
+          "type": "vector",
+          "numDimensions": 1536,
+          "path": "embedding",
+          "similarity": "cosine"
+        }
+      ]
+    },
+    name = index_name,
+    type = "vectorSearch"
+  )
+  collection.create_search_index(model=search_index_model)
+
+  # Wait for initial sync to complete
+  print("Polling to check if the index is ready. This may take up to a minute.")
+  predicate=None
+  if predicate is None:
+    predicate = lambda index: index.get("queryable") is True
+  while True:
+    indices = list(collection.list_search_indexes(index_name))
+    if len(indices) and predicate(indices[0]):
+        break
+    time.sleep(5)
+  print(index_name + " is ready for querying.")
+
 def num_tokens_from_string(doc: str) -> int:
     # Initialize the tokenizer for a specific model (e.g., OpenAI GPT-3.5-turbo)
     tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo")
@@ -80,7 +117,8 @@ if __name__ == "__main__":
   # Store in MongoDB
   result = insert_docs_to_mongodb(docs_to_insert)
 
-  print(result)
+  # Create Atlas Vector Search index
+  create_vector_index()
   
 
   

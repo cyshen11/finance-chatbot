877e89a930673136c2089c7fa86540a6414c1ff6
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-11-28 22:39:05 +0800

    use that pip-installed package instead of the built-in sqlite3

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from components.graph import build_graph
 from components.vector_store import create_vector_store

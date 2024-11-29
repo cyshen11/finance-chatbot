7ac6025bb0dcfeecf7c5729b5fb4fb28c67ec41f
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-11-29 16:32:42 +0800

    added documentation

--- components/generator.py
+++ components/generator.py
@@ -1,26 +1,34 @@
+"""Generator to generate response based on retrieved docs"""
 
 from langchain import hub
 from langchain_openai import ChatOpenAI
 from components.utils import State
 
-# Post-processing
-def format_docs(docs):
-    return "\n\n".join(doc["text"] for doc in docs)
-
 def generate(state:State):
+  """Generate response based on retrieved docs
+
+  Args:
+      state (State): RAG State (defined in utils.py)
+
+  Returns:
+      Response (Object): Response object contains answer and source
+  """
+
   llm = ChatOpenAI(model="gpt-4o-mini")
-  
-  # Define prompt for question-answering
   prompt = hub.pull("rlm/rag-prompt")
   
+  # Check if no retrieved docs, return no context
   if not state["context"]:
      return {"answer": "no context"}
 
+  # Post-processing retrieved docs
   docs_content = "\n\n".join(doc.page_content for doc in state["context"])
   docs_source = "\n\n".join( 'page:' + str(doc.metadata['page']) + '\nsource:' + doc.metadata['source'] for doc in state["context"])
 
+  # Generate prompt
   messages = prompt.invoke({"question": state["question"], "context": docs_content})
   
+  # Generate answer
   response = llm.invoke(messages)
   
   return {"answer": response.content, "source": docs_source}

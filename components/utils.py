a581dbdbbae94df9f581e4ef2d41ee6fac1d0ea7
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-04 21:22:51 +0800

    removed context and added query

--- components/utils.py
+++ components/utils.py
@@ -1,18 +1,17 @@
 """Utilities that define the RAG State class"""
 
-from typing_extensions import List, TypedDict
-from langchain_core.documents import Document
+from typing_extensions import TypedDict
 
 class State(TypedDict):
     """RAG State
 
     Attrs:
         question (str): User question
-        context (list(Document)): Retrieved list of documents from ChromaDB
+        query (str): Generated query
         answer (str): Generated answer
         source (str): Document source link
     """
     question: str
-    context: List[Document]
+    query: str
     answer: str
     source: str

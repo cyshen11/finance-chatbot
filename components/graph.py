61e8943074dd8568d63f76b0cd9c6f6d19efe723
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-05 23:33:58 +0800

    updated to run_graph

--- components/graph.py
+++ components/graph.py
@@ -4,6 +4,7 @@ from langgraph.graph import START, StateGraph
 from langgraph.checkpoint.memory import MemorySaver
 from components.writer import write_query
 from components.executor import execute_query
+from components.generator import generate_answer
 from components.utils import State
 import json
 
@@ -14,17 +15,22 @@ def build_graph():
       Graph (Graph): RAG Graph
   """
   memory = MemorySaver()
-  graph_builder = StateGraph(State).add_sequence([write_query, execute_query])
+  graph_builder = StateGraph(State).add_sequence([write_query, execute_query, generate_answer])
   graph_builder.add_edge(START, "write_query")
   graph = graph_builder.compile(checkpointer=memory, interrupt_before=["execute_query"])
 
   return graph
 
-def graph_write_query(graph, question):
+def run_graph(graph, question, mode):
   config = {"configurable": {"thread_id": "1"}}
 
+  if mode == "write_query":
+    input = {"question": question}
+  else:
+    input = None
+
   for step in graph.stream(
-      {"question": question},
+      input,
       config,
       stream_mode="updates",
   ):

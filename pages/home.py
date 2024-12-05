603336ce66b24294acc415a055e11b9f045c4d36
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-05 23:23:43 +0800

    updated for graph to write query and pause

--- pages/home.py
+++ pages/home.py
@@ -2,11 +2,19 @@
 
 import os
 import streamlit as st
-from components.graph import run_graph
+from components.graph import build_graph, graph_write_query
+from langchain_community.callbacks.streamlit import (
+    StreamlitCallbackHandler,
+)
 
 st.title("⚡ SQL Chatbot/RAG")
 
+# Set environment variables
 openai_api_key = os.environ["OPENAI_API_KEY"]
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = st.secrets["LANGSMITH"]["LANGCHAIN_ENDPOINT"]
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGSMITH"]["LANGCHAIN_API_KEY"]
+os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGSMITH"]["LANGCHAIN_PROJECT"]
 
 st.markdown(
     """
@@ -31,4 +39,5 @@ with st.form("my_form"):
     # #   st.text(response["answer"])
     #   st.markdown("### Query")
     #   st.markdown(response['query'])
-    st.write_stream(run_graph(question))
+    graph = build_graph()
+    st.write(graph_write_query(graph, question))

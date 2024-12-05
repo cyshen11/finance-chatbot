3c902774b75c54378daa1b47f5fd23c0db55b906
cyshen11 <cyshen11@gmail.com>
2024-12-05 11:52:24 +0800

    added checkpointer and execute_query

--- components/graph.py
+++ components/graph.py
@@ -1,6 +1,7 @@
 """RAG Graph"""
 
 from langgraph.graph import START, StateGraph
+from langgraph.checkpoint.memory import MemorySaver
 from components.writer import write_query
 from components.utils import State
 
@@ -10,8 +11,9 @@ def build_graph():
   Returns:
       Graph (Graph): RAG Graph
   """
-  graph_builder = StateGraph(State).add_sequence([write_query])
-  graph_builder.add_edge(START, "write_query")
-  graph = graph_builder.compile()
+  memory = MemorySaver()
+  graph_builder = StateGraph(State).add_sequence([write_query, execute_query])
+  graph_builder.add_edge(START, ["write_query", "execute_query"])
+  graph = graph_builder.compile(checkpointer=memory, interrupt_before=["execute_query"])
 
   return graph

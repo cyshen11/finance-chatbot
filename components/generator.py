c8927da926a12fa8b196a8721b051511d744b7a2
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-05 23:28:29 +0800

    init

--- components/generator.py
+++ components/generator.py
@@ -1,53 +1,16 @@
-"""Generator to generate response based on retrieved docs"""
-
-from langchain import hub
+"""Generator to generate response based on corresponding SQL query"""
 from langchain_openai import ChatOpenAI
-from langchain_google_genai import ChatGoogleGenerativeAI
 from components.utils import State
-import os
 
-model = os.environ["model"]
-
-def generate(state:State):
-  """Generate response based on retrieved docs
-
-  Args:
-      state (State): RAG State (defined in utils.py)
-
-  Returns:
-      Response (Object): Response object contains answer and source
-  """
-  if model == "Google Gemini 1.5 Flash-8B":
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-8b")
-  elif model == "OpenAI gpt-4o-mini":
+def generate_answer(state: State):
+    """Answer question using retrieved information as context."""
     llm = ChatOpenAI(model="gpt-4o-mini")
-
-  prompt = hub.pull("rlm/rag-prompt")
-  
-  # Check if no retrieved docs, return no context
-  if not state["context"]:
-     return {"answer": "no context"}
-
-  # Post-processing retrieved docs
-  docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-  
-  # Get only documents with unique source and page
-  unique_documents = []
-  seen = set()
-  for doc in state["context"]:
-    source = doc.metadata.get("source")
-    page = doc.metadata.get("page")
-    identifier = (source, page)
-    if identifier not in seen:
-        unique_documents.append(doc)
-        seen.add(identifier)
-
-  docs_source = "\n\n".join( 'page:' + str(doc.metadata['page']) + '\nsource:' + doc.metadata['source'] for doc in unique_documents)
-
-  # Generate prompt
-  messages = prompt.invoke({"question": state["question"], "context": docs_content})
-  
-  # Generate answer
-  response = llm.invoke(messages)
-  
-  return {"answer": response.content, "source": docs_source}
+    prompt = (
+        "Given the following user question, corresponding SQL query, "
+        "and SQL result, answer the user question.\n\n"
+        f'Question: {state["question"]}\n'
+        f'SQL Query: {state["query"]}\n'
+        f'SQL Result: {state["result"]}'
+    )
+    response = llm.invoke(prompt)
+    return {"answer": response.content}

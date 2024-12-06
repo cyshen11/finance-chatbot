544b5633b9693cf84de65d78b979dee61048daca
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-06 22:52:23 +0800

    added documentation

--- components/executor.py
+++ components/executor.py
@@ -1,10 +1,20 @@
+"""Executor to execute query"""
+
 from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 from components.utils import State
 from langchain_community.utilities import SQLDatabase
 import sqlite3
 
 def execute_query(state: State):
-    """Execute SQL query."""
+    """Initialize SharePoint document loaders
+
+    Args:
+        state (State): Langgraph State
+
+    Returns:
+        result: Object containing the result of the query
+    """
+    # Open database in read-only format
     creator = lambda: sqlite3.connect('file:data/sqlite_db.db?mode=ro', uri=True)
     db = SQLDatabase.from_uri('sqlite:///' , engine_args={'creator': creator})
 
